df446bf74a175b05dd7c6181db4b9cc4780b4cd0
Alex Hubers <ahubers15@cornellcollege.edu>
2014-07-30 10:50:46 -0700

    unworking prototype. Will debug shortly.

--- scripts/color_controller.py
+++ scripts/color_controller.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 import rospy
+
 from sensor_msgs.msg import Image, CameraInfo 
+from tf2_msgs.msg import TFMessage
+from image_geometry import PinholeCameraModel
 from cmvision.msg import Blobs
 
 from cv_bridge import CvBridge
@@ -13,8 +16,8 @@ from color_model import color_model
 
 #This package integrates cmvision with tf and localization; now we can track color in 3D.
 class color_controller():
-    def __init__(self, depth_topic, info_topic, map_topic):
-        #We are using a depth image to get depth information what we're tracking.
+    def __init__(self, depth_topic, info_topic, parent_frame):
+        #We are using a depth image to get depth information of what we're tracking.
         rospy.Subscriber(depth_topic, Image, self.depth_callback)
 
         #We need CameraInfo in order to use PinholeCameraModel below.
@@ -24,22 +27,44 @@ class color_controller():
         #This package is just an extension of cmvision to provide tf tracking of the blobs provided by cmvision. 
         rospy.Subscriber('blobs', Blobs, self.blob_callback)
 
+        #Send to our view.
+        self.view_pub = rospy.Publisher('color_tracker/tf', TFMessage)
+
         #To take our Ros Image into a cv message and subsequently a numpy array.
         self.bridge = CvBridge()        
 
         # To make the pixel to vector projection
-        self.camModel = PinholeCameraModel()
+        self.cam_model = PinholeCameraModel()
 
         #We are integrating cmvision with localization and thus want to know which frame we should put our colors into.
-        self.map_topic = map_topic
+        self.parent_frame = parent_frame
 
         #array of our color_models.
-        self.colors = []
+        self.colors = {}
 
+        #Keep track of our transforms to send them to color_broadcaster.
+        self.transforms = TFMessage()
+
+        #blobs is received from running cmvision. It's color blobs as defined by our color file.
     def blob_callback(self, blobs):
-        for blob in blobs.blobs:
-            pass
+        self.transforms = TFMessage()
+        wasUpdated = False
+        try:
+            for blob in blobs.blobs:
+                #If this blob already exists, update our idea of it. Otherwise create another color_model.
+                if self.colors[blob.name]:
+                    if (self.colors[blob.name].update(blob, self.depth_image, self.cam_model)):
+                        wasUpdated = True
+                else:
+                    self.colors[blob.name] = color_model(blob, self.parent_frame, self.depth_image, self.camModel)
+        
+                #Publish it whether it's been updated or not.
+                self.transforms.transforms.append(self.colors[blob.name].publish() )
 
+        except():
+            pass
+        if wasUpdated:
+            self.view_pub.publish(self.transforms)
 
     def depth_callback(self, image):
         image_cv = self.bridge.imgmsg_to_cv(image, '32FC1')
@@ -49,12 +74,15 @@ class color_controller():
 
     def camera_callback(self, info):
         if not self.hasCameraInfo:
-            self.camModel.fromCameraInfo(cameraInfo)
+            self.cam_model.fromCameraInfo(cameraInfo)
         self.hasCameraInfo = True
 
 
 if __name__ == '__main__':
     rospy.init_node('color_controller')
-    boundingBox = color_controller('camera/depth/image', '/camera/rgb/camera_info', '/map')
+    depth_image = rospy.get_param("color_controller/depth_image", "camera/depth/image")
+    camera_topic = rospy.get_param("color_controller/camera_topic", '/camera/rgb/camera_info')
+    parent_frame = rospy.get_param("color_controller/parent_frame", '/camera_rgb_optical_frame')
+    boundingBox = color_controller('camera/depth/image', '/camera/rgb/camera_info', '/camera_rgb_optical_frame')
 
     rospy.spin()
--- scripts/color_model.py
+++ scripts/color_model.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 #!/usr/bin/env python
 import rospy
-from cmvision.msg import Blobs
+from image_geometry import PinholeCameraModel
+from tf2_msgs.msg import TFMessage, TransformStamped
+from cmvision.msg import Blobs, Blob
 
 import cv, cv2
 import numpy as np
@@ -9,13 +11,43 @@ import numpy as np
 #The "model" part of our model-view-controller. 
 #Each color_model represents a color that we're tracking. When calling update(), you present new information.
 class color_model():
-    def __init__(self):
-        pass
-
+    def __init__(self, blob, parent_frame, depth_image, cam_model):
+        self.blob = blob
+        self.parent_frame = parent_frame
+        self.depth_image = depth_image
+        self.cam_model = cam_model
     #Updates the model with more information. Returns false if this information is rejected and true if this information is accepted.
-    def update():
-        pass
+    def update(blob, depth_image, cam_model):
+        return True
 
     #Publishes to our view, color_broadcaster, if the model updates. 
     def publish():
-        pass
+    	return self._toTransform()
+## Private functions
+## ^^^^^^^^^^^^^^^^^
+	
+	#Takes our data and makes a tf2 transform message.
+	def _toTransform():
+		transform = TransformStamped()
+		transform.header.stamp = rospy.Time.now()
+		transform.header.frame_id = self.parent_frame
+		transform.child_frame_id = self.blob.name
+
+		(x,y,z) = self._projectTo3d(self.blob.x, self.blob.y)
+		transform.transform.translation.x = x
+		transform.transform.translation.y = y
+		transform.transform.translation.z = z
+
+		transform.transform.rotation.w = 1.0
+
+	def _projectTo3d(x, y):
+		[vx,vy,vz] = self.cam_model.projectPixelTo3dRay((x,y))
+		blob_z = self._getDepthAt(x,y)
+		blob_x = vx * blob_z
+		blob_y = vy * blob_z
+
+		return (blob_x, blob_y, blob_z)
+
+	def _getDepthAt(x,y):
+		return self.depth_image[y][x]
+
